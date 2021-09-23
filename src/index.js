a8bc6f97540fa00149ec4af2890f781f78c98505
PHONG VO <61008063+PETER-VO@users.noreply.github.com>
2021-09-23 18:43:06 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -10,4 +10,12 @@ if (document.readyState !== "loading") {
 
 function initializeCode() {
   document.getElementById("app").innerHTML = "<h1>Hello!</h1>";
+  fetch("https://dog.ceo/api/breeds/image/random").then((result) => {
+    result.json().then((data) => {
+      var img = document.createElement("img");
+      img.setAttribute("class", "wiki-img");
+      img.setAttribute("src", data.message);
+      document.getElementById("img-container").appendChild(img);
+    });
+  });
 }
